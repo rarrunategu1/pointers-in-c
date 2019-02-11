9d3478ad979aba33c888ef39b15bc3c62b1e59d6
Rose Parker <parker.rose@ymail.com>
2019-02-11 00:20:25 +0000

    Pop will pop the head and free it if there is only one node pointer value.  Still receiving a memory leak

--- list.c
+++ list.c
@@ -10,6 +10,7 @@ typedef struct Node
 void push(int, struct Node*);
 int pop(struct Node*);
 int peek(struct Node*);
+void freeList(Node*);
 
 int main(void)
 {
@@ -27,11 +28,12 @@ int main(void)
 
     //give each node their data
     head->data = 5;
-    head->next = second;
-    second->data = 7;
-    second->next = third;
-    third->data = 9;
-    third->next = NULL;
+    head->next = NULL;
+    // head->next = second;
+    // second->data = 7;
+    // second->next = third;
+    // third->data = 9;
+    // third->next = NULL;
 
     //same as:
     // head->data = 5;
@@ -42,10 +44,10 @@ int main(void)
     // head->next->next->next = NULL;
 
     //adds 11 to the end of the list
-    push(11, head);
-    printf("Popped value at end of list is: %i\n", pop(head));
-    printf("Peep last value at end of list now.  It is: %i\n", peek(head));
-    //int result = peek;
+    //push(11, head);
+    printf("The last value at the end of list removed is: %i\n", pop(head));
+    //printf("Peep last value at end of list now.  It is: %i\n", peek(head));
+    //freeList(head);
 
 }
 
@@ -73,27 +75,37 @@ void push(int data, struct Node* head)
     // newNode->next = NULL;
     // trav->next = newNode;
 }
+
+//get pop to work for a single item
 int pop(struct Node* head)
-//navigate to end of list
-//traveler points to second to last item of list
-//free last item
-//update second to last item to be the last itme
 {
     struct Node* trav = head;
-    //int counter = 0;
+    if(trav->next == NULL)
+        {
+            int data = trav->data;
+            free(trav);
+            printf("%p\n", trav->next);
+            printf("%p\n", trav);
+            trav = NULL;
+            //printf("%p\n", trav->next);
+            printf("%p\n", trav);
+            //printf("%i\n", trav->data);
 
-
-    //find second to last node
+            return data;
+        }
     while(trav->next->next != NULL)
     {
         trav = trav->next;
     }
 
     int data = trav->next->data;
+    //FREES LAST ITEM WHICH IS EQUAL TO NULL
     free(trav->next);
+    //ASSIGNS THE NOW LAST ITEM TO NULL
     trav->next = NULL;
 
     return data;
+
 
 }
 
@@ -111,5 +123,34 @@ int peek(struct Node* head)
     //printf("%i\n", data);
     return data;
 
+}
+
+void freeList(Node* root)
+{
+    Node* trav = root;
+
+    //if only one item
+    if (trav->next == NULL)
+    {
+        free(trav);
+        return;
+    }
+    if (trav->next->next == NULL)
+    {
+        //printf("%i\n", trav->next->data);
+        free(trav->next);
+        free(trav);
+        //printf("%i\n", trav->next->data);
+        return;
+    }
+    //printf("%i\n", trav->next->data);
+    freeList(trav->next);
+    //printf("%i\n", trav->next->data);
+    //frees head
+    free(trav);
+
+
+    // int data = trav->data;
+    // return data;
 }
 
